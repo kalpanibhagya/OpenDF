09c0bf04bf321da2c753dd53c75095dffd2d90ca
Janakshi28 <janakshid28@gmail.com>
2017-09-03 11:25:49 +0530

    Add datalist for the demo

--- OpenDF-FileBrowser/file-browser/src/App.js
+++ OpenDF-FileBrowser/file-browser/src/App.js
@@ -14,6 +14,323 @@ import './App.css';
 
 injectTapEventPlugin()
 
+const listItems = [{  
+      depth:0,
+      children:[  
+
+      ],
+      _styles:{  
+         root:{  
+            paddingLeft:-16,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false
+   },
+   {  
+      title:"Downloads",
+      depth:1,
+      parentIndex:0,
+      children:[  
+         2,
+         5
+      ],
+      disabled:false,
+      _styles:{  
+         root:{  
+            paddingLeft:0,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:true,
+      _primaryText:"Downloads"
+   },
+   {  
+      title:"React Tutorial",
+      depth:2,
+      children:[  
+         3,
+         4
+      ],
+      parentIndex:1,
+      disabled:false,
+      _styles:{  
+         root:{  
+            paddingLeft:16,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"React Tutorial"
+   },
+   {  
+      title:"Redux-saga Getting Started",
+      depth:3,
+      parentIndex:2,
+      disabled:false,
+      content:"Redux-saga is a redux middleware library, that is designed to make handling side effects in your redux app nice and simple. It achieves this by leveraging an ES6 feature called Generators, allowing us to write asynchronous code that looks synchronous, and is very easy to test.",
+      _styles:{  
+         root:{  
+            paddingLeft:32,
+            backgroundColor:"rgba(0,0,0,0.2)",
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Redux-saga Getting Started"
+   },
+   {  
+      title:"Git Tutorial",
+      depth:3,
+      parentIndex:2,
+      disabled:false,
+      content:"Git is currently the most popular implementation of a distributed version control system. Git originates from the Linux kernel development and was founded in 2005 by Linus Torvalds. Nowadays it is used by many popular open source projects, e.g., the Android or the Eclipse developer teams, as well as many commercial organizations.The core of Git was originally written in the programming language _C, but Git has also been re-implemented in other languages, e.g., Java, Ruby and Python.",
+      _styles:{  
+         root:{ 
+            paddingLeft:32,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Git Tutorial"
+   },
+   {  
+      title:"Testing React Apps",
+      depth:2,
+      children:[  
+         6,
+         7
+      ],
+      disabled:false,
+      parentIndex:1,
+      _styles:{  
+         root:{  
+            paddingLeft:16,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Testing React Apps"
+   },
+   {  
+      title:"React-boilerplate",
+      depth:3,
+      parentIndex:5,
+      disabled:false,
+      content:"React is a front-end library developed by Facebook. It is used for handling the view layer for web and mobile apps. ReactJS allows us to create reusable UI components. It is currently one of the most popular JavaScript libraries and has a strong foundation and large community behind it.",
+      _styles:{  
+         root:{  
+            "paddingLeft":32,
+            "backgroundColor":null,
+            "height":"48px",
+            "cursor":"pointer",
+            "color":"rgba(0,0,0,0.87)",
+            "overflow":"hidden",
+            "transform":"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"React-boilerplate"
+   },
+   {  
+      title:"Introduction to J2EE",
+      depth:3,
+      parentIndex:5,
+      disabled:false,
+      content:"React is a front-end library developed by Facebook. It is used for handling the view layer for web and mobile apps. ReactJS allows us to create reusable UI components. It is currently one of the most popular JavaScript libraries and has a strong foundation and large community behind it.",
+      _styles:{  
+         root:{  
+            paddingLeft:32,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Introduction to J2EE"
+   },
+   {  
+      title:"Documents",
+      depth:1,
+      parentIndex:0,
+      disabled:false,
+      children:[  
+         9,
+         12
+      ],
+      _styles:{  
+         root:{  
+            paddingLeft:0,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:true,
+      _primaryText:"Documents"
+   },
+   {  
+      title:"Introduction to React",
+      depth:2,
+      parentIndex:8,
+      disabled:false,
+      children:[  
+         10,
+         11
+      ],
+      _styles:{  
+         root:{  
+            paddingLeft:16,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Introduction to React"
+   },
+   {  
+      title:"Writing React Components",
+      depth:3,
+      parentIndex:9,
+      disabled:false,
+      content:"React is a front-end library developed by Facebook. It is used for handling the view layer for web and mobile apps. ReactJS allows us to create reusable UI components. It is currently one of the most popular JavaScript libraries and has a strong foundation and large community behind it.",
+      _styles:{  
+         root:{  
+            paddingLeft:32,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Writing React Components"
+   },
+   {  
+      title:"Composing React Components",
+      depth:3,
+      parentIndex:9,
+      disabled:false,
+      content:"React is a front-end library developed by Facebook. It is used for handling the view layer for web and mobile apps. ReactJS allows us to create reusable UI components. It is currently one of the most popular JavaScript libraries and has a strong foundation and large community behind it.",
+      _styles:{  
+         root:{  
+            paddingLeft:32,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"pointer",
+            color:"rgba(0,0,0,0.87)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Composing React Components"
+   },
+   {  
+      title:"React Components",
+      depth:2,
+      parentIndex:8,
+      disabled:true,
+      children:[  
+         13,
+         14
+      ],
+      _styles:{  
+         root:{  
+            paddingLeft:16,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"not-allowed",
+            color:"rgba(0,0,0,0.4)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"React Components"
+   },
+   {  
+      title:"Props and State in React",
+      parentIndex:12,
+      depth:3,
+      disabled:true,
+      content:"React is a front-end library developed by Facebook. It is used for handling the view layer for web and mobile apps. ReactJS allows us to create reusable UI components. It is currently one of the most popular JavaScript libraries and has a strong foundation and large community behind it.",
+      _styles:{  
+         root:{  
+            paddingLeft:32,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"not-allowed",
+            color:"rgba(0,0,0,0.4)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Props and State in React"
+   },
+   {  
+      title:"Component Lifecycle",
+      parentIndex:12,
+      depth:3,
+      disabled:true,
+      content:"React is a front-end library developed by Facebook. It is used for handling the view layer for web and mobile apps. ReactJS allows us to create reusable UI components. It is currently one of the most popular JavaScript libraries and has a strong foundation and large community behind it.",
+      _styles:{  
+         root:{  
+            paddingLeft:32,
+            backgroundColor:null,
+            height:"48px",
+            cursor:"not-allowed",
+            color:"rgba(0,0,0,0.4)",
+            overflow:"hidden",
+            transform:"translateZ(0)"
+         }
+      },
+      _shouldRender:false,
+      _primaryText:"Component Lifecycle"
+   }];
+
 const files = listItems
   .map((listItem, i) => {
     if (!listItem.children) {
@@ -156,7 +473,7 @@ class App extends Component {
               handleSearch={this.handleSearch}
               searchTerm={searchTerm}
               >
-              <Subheader>Material UI Version</Subheader>
+              <Subheader>OpenDF - File Browser</Subheader>
             </MuiTreeList>      
     )
   } 
